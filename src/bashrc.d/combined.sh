ac955e49f94a35aaebafbec714f7ffddf39e4df1
CJ Oster <cj@cjoster.com>
2023-05-11 14:03:18 -0500

    Silences a pactl call

--- src/bashrc.d/combined.sh
+++ src/bashrc.d/combined.sh
@@ -1,1 +1,1 @@
-[ -n "$(pactl list short sinks | awk '$2=="combined"{print $1}')" ] || pactl load-module module-combine-sink; [ -n "$(pactl list short sinks | awk '$2=="combined"{print $1}')" ] && pactl set-default-sink combined  || { >&2 echo "Error, cannot create combined output sink."; }
+[ -n "$(pactl list short sinks | awk '$2=="combined"{print $1}')" ] || pactl load-module module-combine-sink > /dev/null 2>&1; [ -n "$(pactl list short sinks | awk '$2=="combined"{print $1}')" ] && pactl set-default-sink combined  || { >&2 echo "Error, cannot create combined output sink."; }
